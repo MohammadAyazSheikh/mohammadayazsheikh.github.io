b6346e91f12b3690f93108101e6e85da2abfd0a0
Mohammad Ayaz <mohammadayazsheikh@gmail.com>
2023-10-18 13:12:04 +0500

    some fixes

--- script/animation.js
+++ script/animation.js
@@ -1,388 +1,297 @@
-const pushText = (text) => {
+let skills = [
+  "React Native Developer",
+  "Nodejs Developer",
+  "Frontend Developer",
+];
 
-    let i = 0;
+const pushText = (text, skillIndex_) => {
+  let i = 0;
+  let skillIndex = skillIndex_;
 
-    const id = setInterval(() => {
+  const id = setInterval(() => {
+    const txtSkill = document.querySelector(".txtSkills");
 
-        const txtSkill = document.querySelector('.txtSkills');
+    if (text[i]) txtSkill.innerHTML += text[i];
 
-        if (text[i])
-            txtSkill.innerHTML += text[i];
+    i++;
 
-        i++;
-
-        if (i > text.length) {
-            i = 0;
-            clearInterval(id);
-        }
-
-    }, 200);
-}
-
-
-const AnimateSkills = () => {
-
-    let skills = ["React Native Developer", "Nodejs Developer", "Frontend Developer"];
-
-    pushText(skills[2]);
-    var indexSkills = 0;
-    let time = 23 * 200;
-    const id = setInterval(() => {
-
-
-        if (indexSkills > skills.length) {
-            indexSkills = 0;
-
+    if (i > text.length) {
+    //adding delay of 3 secs so the user can read complete text before it resets
+      setTimeout(() => {
+        txtSkill.innerHTML = "";
+        skillIndex++;
+        if (skillIndex < skills.length) {
+          pushText(skills[skillIndex], skillIndex);
         } else {
-
-            if (skills[indexSkills]) {
-                pushText(skills[indexSkills]);
-                const txtSkill = document.querySelector('.txtSkills');
-                txtSkill.innerHTML = "";
-            } else {
-                pushText(skills[1]);
-                const txtSkill = document.querySelector('.txtSkills');
-                txtSkill.innerHTML = "";
-            }
-            indexSkills++;
+          skillIndex = 0;
+          pushText(skills[skillIndex], skillIndex);
         }
+      }, 3000);
 
-    }, time);
+      clearInterval(id);
+    }
+  }, 200);
+};
 
-}
-
-AnimateSkills();
+pushText(skills[0], 0);
 
 
 document.getElementById("btnMenu").addEventListener("click", () => {
-    document.querySelector('.navBar').style.display = 'flex';
-    // document.querySelector('.navBar').style.right = '100%';
-
+  document.querySelector(".navBar").style.display = "flex";
+  // document.querySelector('.navBar').style.right = '100%';
 });
 
 document.getElementById("btnClose").addEventListener("click", () => {
-    document.querySelector('.navBar').style.display = 'none';
-    // document.querySelector('.navBar').style.right = '-100%';
+  document.querySelector(".navBar").style.display = "none";
+  // document.querySelector('.navBar').style.right = '-100%';
 });
 
-
-
-
 const changePercentage = (el, max) => {
-    let percentage = 0;
+  let percentage = 0;
 
-    const id = setInterval(() => {
+  const id = setInterval(() => {
+    percentage++;
+    el.innerHTML = `${percentage}%`;
 
-        percentage++;
-        el.innerHTML = `${percentage}%`;
-
-        if (percentage === max)
-            clearInterval(id);
-
-    }, 20);
-}
-
-
+    if (percentage === max) clearInterval(id);
+  }, 20);
+};
 
 const animatePercentage = () => {
-    let options = {
-        root: null, // view port  ///document.querySelector('#scrollArea'),
-        rootMargin: '0px',
-        threshold: 0.3
-    }
+  let options = {
+    root: null, // view port  ///document.querySelector('#scrollArea'),
+    rootMargin: "0px",
+    threshold: 0.3,
+  };
 
-    let observer = new IntersectionObserver((entries, observer) => {
+  let observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (!entry.isIntersecting) return;
 
-        entries.forEach(entry => {
-            if (!entry.isIntersecting)
-                return;
+      console.log(entry.target);
 
-            // console.log(entry.target);
+      // document.querySelector(".navBar").style.backgroundColor = "red";
+      const skillTextEl = entry.target.firstElementChild.lastElementChild;
 
+      const id = entry.target.getAttribute("id");
 
-            // document.querySelector(".navBar").style.backgroundColor = "red";
-            const skillTextEl = entry.target.firstElementChild.lastElementChild;
+      switch (id) {
+        case "c1":
+          changePercentage(skillTextEl, 70);
+          document.querySelector(".c1").style.strokeDashoffset =
+            376.99 - (376.99 / 100) * 70;
+          break;
+        case "c2":
+          changePercentage(skillTextEl, 75);
+          document.querySelector(".c2").style.strokeDashoffset =
+            376.99 - (376.99 / 100) * 75;
+          break;
+        case "c3":
+          changePercentage(skillTextEl, 60);
+          document.querySelector(".c3").style.strokeDashoffset =
+            376.99 - (376.99 / 100) * 60;
+          break;
+        case "c4":
+          changePercentage(skillTextEl, 65);
+          document.querySelector(".c4").style.strokeDashoffset =
+            376.99 - (376.99 / 100) * 65;
+          break;
+        case "c5":
+          changePercentage(skillTextEl, 85);
+          document.querySelector(".c5").style.strokeDashoffset =
+            376.99 - (376.99 / 100) * 85;
+          break;
+        case "c6":
+          changePercentage(skillTextEl, 80);
+          document.querySelector(".c6").style.strokeDashoffset =
+            376.99 - (376.99 / 100) * 80;
+          break;
+      }
 
-            const id = entry.target.getAttribute("id");
+      observer.unobserve(entry.target);
+    });
+  }, options);
 
-            switch (id) {
-                case "c1":
-                    changePercentage(skillTextEl, 70);
-                    document.querySelector(".c1").style.strokeDashoffset = 376.99 - (376.99 / 100) * 70;
-                    break;
-                case "c2":
-                    changePercentage(skillTextEl, 75);
-                    document.querySelector(".c2").style.strokeDashoffset = 376.99 - (376.99 / 100) * 75;
-                    break;
-                case "c3":
-                    changePercentage(skillTextEl, 60);
-                    document.querySelector(".c3").style.strokeDashoffset = 376.99 - (376.99 / 100) * 60;
-                    break;
-                case "c4":
-                    changePercentage(skillTextEl, 65);
-                    document.querySelector(".c4").style.strokeDashoffset = 376.99 - (376.99 / 100) * 65;
-                    break;
-                case "c5":
-                    changePercentage(skillTextEl, 85);
-                    document.querySelector(".c5").style.strokeDashoffset = 376.99 - (376.99 / 100) * 85;
-                    break;
-                case "c6":
-                    changePercentage(skillTextEl, 80);
-                    document.querySelector(".c6").style.strokeDashoffset = 376.99 - (376.99 / 100) * 80;
-                    break;
-            }
+  let singleSkillDiv = document.querySelectorAll(".singleSkillDiv");
 
-            observer.unobserve(entry.target);
-
-
-        });
-    }, options);
-
-    let singleSkillDiv = document.querySelectorAll('.singleSkillDiv');
-
-
-    singleSkillDiv.forEach(item => {
-        observer.observe(item);
-    })
-}
-
-
-
+  singleSkillDiv.forEach((item) => {
+    observer.observe(item);
+  });
+};
 
 animatePercentage();
 
-
 const animateNav = () => {
-    let options = {
-        root: null, // view port  ///document.querySelector('#scrollArea'),
-        rootMargin: '-100px',
-        threshold: 0.4
-    }
+  let options = {
+    root: null, // view port  ///document.querySelector('#scrollArea'),
+    rootMargin: "-100px",
+    threshold: 0.4,
+  };
 
-    let observer = new IntersectionObserver((entries, observer) => {
+  let observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (!entry.isIntersecting) return;
 
-        entries.forEach(entry => {
-            if (!entry.isIntersecting)
-                return;
+      console.log(entry.target);
 
-            console.log(entry.target);
+      document.querySelector(".navBar").classList.toggle("changeNavColor");
 
+      // observer.unobserve(entry.target);
+    });
+  }, options);
 
-            document.querySelector(".navBar").classList.toggle("changeNavColor")
+  let SkillDiv = document.querySelector(".skillsDiv");
 
-            // observer.unobserve(entry.target);
-
-
-        });
-    }, options);
-
-    let SkillDiv = document.querySelector('.skillsDiv');
-
-
-    observer.observe(SkillDiv);
-
-}
+  observer.observe(SkillDiv);
+};
 
 // animateNav();
 
-
 const animateWork = () => {
+  const workItems = document.querySelectorAll(".whatIDoItem");
 
-    const workItems = document.querySelectorAll('.whatIDoItem');
+  let options = {
+    root: null, // view port  ///document.querySelector('#scrollArea'),
+    rootMargin: "-50px",
+    threshold: 0.3,
+  };
 
-    let options = {
-        root: null, // view port  ///document.querySelector('#scrollArea'),
-        rootMargin: '-50px',
-        threshold: 0.3
-    }
+  let observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (!entry.isIntersecting) return;
 
-    let observer = new IntersectionObserver((entries, observer) => {
+      // console.log(entry.target);
 
-        entries.forEach(entry => {
-            if (!entry.isIntersecting)
-                return;
+      workItems.forEach((item) => {
+        item.classList.toggle("workAnim");
+      });
 
-            // console.log(entry.target);
+      observer.unobserve(entry.target);
+    });
+  }, options);
 
-            workItems.forEach(item => {
-                item.classList.toggle("workAnim");
-            })
-
-
-            observer.unobserve(entry.target);
-
-
-        });
-    }, options);
-
-
-
-
-
-    workItems.forEach(item => {
-        observer.observe(item);
-    })
-
-}
+  workItems.forEach((item) => {
+    observer.observe(item);
+  });
+};
 
 animateWork();
 
-
-
 const animateProgressBar = () => {
+  const aboutMeContent = document.querySelector(".aboutMeContent");
+  const progressBar = document.querySelectorAll(".progressBarFront");
 
-    const aboutMeContent = document.querySelector('.aboutMeContent');
-    const progressBar = document.querySelectorAll('.progressBarFront');
+  let options = {
+    root: null, // view port  ///document.querySelector('#scrollArea'),
+    rootMargin: "-50px",
+    threshold: 0.3,
+  };
 
+  let observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (!entry.isIntersecting) return;
 
-    let options = {
-        root: null, // view port  ///document.querySelector('#scrollArea'),
-        rootMargin: '-50px',
-        threshold: 0.3
-    }
+      // console.log(entry.target);
 
-    let observer = new IntersectionObserver((entries, observer) => {
+      progressBar.forEach((bar) => {
+        const id = bar.getAttribute("id");
 
-        entries.forEach(entry => {
-            if (!entry.isIntersecting)
-                return;
+        if (id == "english") {
+          bar.style.width = "75%";
+        }
 
-            // console.log(entry.target);
+        if (id == "urdu") {
+          bar.style.width = "100%";
+        }
+      });
 
-            progressBar.forEach(bar => {
-                const id = bar.getAttribute("id");
+      observer.unobserve(entry.target);
+    });
+  }, options);
 
-                if (id == "english") {
-                    bar.style.width = "75%";
-                }
-
-                if (id == "urdu") {
-                    bar.style.width = "100%";
-                }
-            })
-
-
-            observer.unobserve(entry.target);
-
-
-        });
-    }, options);
-
-
-    observer.observe(aboutMeContent);
-}
+  observer.observe(aboutMeContent);
+};
 
 animateProgressBar();
 
-
-
 const animateABoutBack = () => {
+  const aboutMeDiv = document.querySelector(".aboutMeDiv");
+  const aboutMeBackDiv = document.querySelector(".aboutBackStyleDiv");
 
-    const aboutMeDiv = document.querySelector('.aboutMeDiv');
-    const aboutMeBackDiv = document.querySelector('.aboutBackStyleDiv');
+  let options = {
+    root: null, // view port  ///document.querySelector('#scrollArea'),
+    rootMargin: "-50px",
+    threshold: 0.1,
+  };
 
+  let observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (!entry.isIntersecting) return;
 
-    let options = {
-        root: null, // view port  ///document.querySelector('#scrollArea'),
-        rootMargin: '-50px',
-        threshold: 0.1
-    }
+      aboutMeBackDiv.style.transform = "rotate(35deg)";
+      // aboutMeBackDiv.style.transform = "scale(1)";
 
-    let observer = new IntersectionObserver((entries, observer) => {
+      observer.unobserve(entry.target);
+    });
+  }, options);
 
-        entries.forEach(entry => {
-            if (!entry.isIntersecting)
-                return;
-
-
-            aboutMeBackDiv.style.transform = "rotate(35deg)";
-            // aboutMeBackDiv.style.transform = "scale(1)";
-
-
-            observer.unobserve(entry.target);
-
-
-        });
-    }, options);
-
-
-    observer.observe(aboutMeDiv);
-}
+  observer.observe(aboutMeDiv);
+};
 
 animateABoutBack();
 
-
-
-
-
-
-
 const animateSkillBack = () => {
+  const skillDiv = document.querySelector(".skillsDiv");
+  const skillBackDiv = document.querySelector(".skillBackDiv");
 
-    const skillDiv = document.querySelector('.skillsDiv');
-    const skillBackDiv = document.querySelector('.skillBackDiv');
+  let options = {
+    root: null, // view port  ///document.querySelector('#scrollArea'),
+    rootMargin: "-50px",
+    threshold: 0.1,
+  };
 
+  let observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (!entry.isIntersecting) return;
 
-    let options = {
-        root: null, // view port  ///document.querySelector('#scrollArea'),
-        rootMargin: '-50px',
-        threshold: 0.1
-    }
+      skillBackDiv.style.transform = "rotate(35deg) scale(2)";
+      // aboutMeBackDiv.style.transform = "scale(1)";
 
-    let observer = new IntersectionObserver((entries, observer) => {
+      observer.unobserve(entry.target);
+    });
+  }, options);
 
-        entries.forEach(entry => {
-            if (!entry.isIntersecting)
-                return;
-
-
-            skillBackDiv.style.transform = "rotate(35deg) scale(2)";
-            // aboutMeBackDiv.style.transform = "scale(1)";
-
-
-            observer.unobserve(entry.target);
-
-
-        });
-    }, options);
-
-
-    observer.observe(skillDiv);
-}
+  observer.observe(skillDiv);
+};
 
 animateSkillBack();
 
-
 const icons = [
-    "fa-brands fa-node-js",
-    "fa-brands fa-react",
-    "fa-solid fa-database",
-    "fa-brands fa-js",
-    "fa-solid fa-code",
-]
+  "fa-brands fa-node-js",
+  "fa-brands fa-react",
+  "fa-solid fa-database",
+  "fa-brands fa-js",
+  "fa-solid fa-code",
+];
 const AnimateHeader = () => {
+  const icon = document.createElement("i");
 
-    const icon = document.createElement("i");
+  icon.style.left = `${Math.floor(Math.random() * 100)}%`;
+  icon.style.top = `${Math.floor(Math.random() * 100)}%`;
 
-    icon.style.left = `${Math.floor(Math.random() * 100)}%`;
-    icon.style.top = `${Math.floor(Math.random() * 100)}%`;
+  const index = Math.floor(Math.random() * 100) % icons.length;
+  icon.className = `${icons[index]} headerAnimatedIcon`;
 
+  const header = document.getElementById("headerSection").appendChild(icon);
 
-    const index = Math.floor(Math.random() * 11) % icons.length;
-    icon.className = `${icons[index]} headerAnimatedIcon`;
+  setTimeout(() => {
+    icon.classList.toggle("animateIcon");
+  }, 100);
 
-    const header = document.getElementById("headerSection").appendChild(icon);
-
-    setTimeout(() => {
-        icon.classList.toggle("animateIcon")
-    }, 100);
-
-    setTimeout(() => {
-        header.remove(icon)
-    }, 2000);
-}
-
-
+  setTimeout(() => {
+    header.remove(icon);
+  }, 2000);
+};
 
 setInterval(AnimateHeader, 1000);
 setInterval(AnimateHeader, 2000);
